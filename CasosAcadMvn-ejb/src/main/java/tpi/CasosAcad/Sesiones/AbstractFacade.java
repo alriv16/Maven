bce8e68a887cc1477057c32309d2c21f0aa20447
manuel <manuel@Manuel>
2017-03-27 10:23:14 -0600

    Abstract Facade con Manejo de Excepciones

--- CasosAcadMvn-ejb/src/main/java/tpi/CasosAcad/Sesiones/AbstractFacade.java
+++ CasosAcadMvn-ejb/src/main/java/tpi/CasosAcad/Sesiones/AbstractFacade.java
@@ -22,7 +22,7 @@ import javax.validation.ValidatorFactory;
  */
 public abstract class AbstractFacade<T> {
 
-    private Class<T> entityClass;
+    private final Class<T> entityClass;
 
     public AbstractFacade(Class<T> entityClass) {
         this.entityClass = entityClass;
@@ -31,19 +31,42 @@ public abstract class AbstractFacade<T> {
     protected abstract EntityManager getEntityManager();
 
     public void create(T entity) {
-        getEntityManager().persist(entity);
+        try{
+        if(entity!=null&&getEntityManager()!=null){
+        getEntityManager().persist(entity);}
+        }catch(Exception e){
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
     }
 
     public void edit(T entity) {
+        try{
+         if(entity!=null&&getEntityManager()!=null)   
         getEntityManager().merge(entity);
+        }catch(Exception e){
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
+        
     }
 
     public void remove(T entity) {
-        getEntityManager().remove(getEntityManager().merge(entity));
+        try{
+            if(entity!=null&&getEntityManager()!=null){
+        getEntityManager().remove(getEntityManager().merge(entity));}
+        }catch(Exception e){
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
     }
 
     public T find(Object id) {
-        return getEntityManager().find(entityClass, id);
+        try{
+           if(id!=null&&getEntityManager()!=null){ 
+        return getEntityManager().find(entityClass, id);}
+        }catch(Exception e){
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        
+        } 
+        return null;
     }
 
     public List<T> findAll() {
@@ -63,20 +86,32 @@ public abstract class AbstractFacade<T> {
     }
 
     public List<T> findRange(int[] range) {
+        
+        try{
         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
         cq.select(cq.from(entityClass));
         javax.persistence.Query q = getEntityManager().createQuery(cq);
         q.setMaxResults(range[1] - range[0] + 1);
         q.setFirstResult(range[0]);
         return q.getResultList();
+        }catch(Exception e){
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
+        return null;
     }
 
     public int count() {
+        
+        try{
         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
         javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
         cq.select(getEntityManager().getCriteriaBuilder().count(rt));
         javax.persistence.Query q = getEntityManager().createQuery(cq);
         return ((Long) q.getSingleResult()).intValue();
+        }catch(Exception e){
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
+        return 0;
     }
     
 }
